b21ef7a0d9406da44f04b65c791a58654c5d7430
Raneem-Hamid <raneemhamid511@gmail.com>
2024-06-10 11:15:36 +0300

    Build : quizMaster program

--- QuizMaster-Challenge/Program.cs
+++ QuizMaster-Challenge/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string PINK = "\u001b[38;5;205m";
+            string RESET = "\u001b[0m";
+            string CYAN = "\u001b[36m";
+            string RED = "\u001b[31m";
+            try
+            {
+                Console.WriteLine($"{PINK}Welcome to the JavaScript Quiz!{RESET}");
+                Console.WriteLine($"{PINK}The test will start in 10 seconds.{RESET}");
+                Thread.Sleep(3000);
+                for (int i = 10; i > 0; i--)
+                {
+                    Console.WriteLine($"{CYAN}Starting in {i}...{RESET}");
+                    Thread.Sleep(1000); 
+                    Console.Clear();
+                }
+                Console.WriteLine("Starting the quiz now...\n");
+                Thread.Sleep(1000);
+                Console.Clear();
+                 QuizMaster.StartQuiz();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"{RED} There is Something wrong please try again!{RESET}");
+            }
+            finally
+            {
+                Console.WriteLine($"{PINK}thank you for using our QuizMaster {RESET}");
+            }
+           
+            
         }
     }
 }
